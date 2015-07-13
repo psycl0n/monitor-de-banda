71f11ce5412891ed368841faa07536db96febdd4
psycl0n <marcelovslello@gmail.com>
2015-07-13 14:10:38 -0700

    Tornando-o scriptável

--- script.sh
+++ script.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 INTERVAL=30
+COMUNITY=$1
+IP=$2
+MIB=$3
 
+echo $COMUNITY $IP $MIB
+OUT1=$(snmpget -v 1 -c $COMUNITY $IP IF-MIB::ifOutOctets.$MIB| awk '{print $4}')
+IN1=$(snmpget -v 1 -c $COMUNITY $IP IF-MIB::ifInOctets.$MIB | awk '{print $4}')
 
-OUT1=$(snmpget -v 1 -c public 192.168.129.202 IF-MIB::ifOutOctets.11| awk '{print $4}')
-IN1=$(snmpget -v 1 -c public 192.168.129.202 IF-MIB::ifInOctets.11 | awk '{print $4}')
 
 sleep $INTERVAL
 
-OUT2=$(snmpget -v 1 -c public 192.168.129.202 IF-MIB::ifOutOctets.11| awk '{print $4}')
-IN2=$(snmpget -v 1 -c public 192.168.129.202 IF-MIB::ifInOctets.11 | awk '{print $4}')
+OUT2=$(snmpget -v 1 -c $COMUNITY $IP IF-MIB::ifOutOctets.$MIB| awk '{print $4}')
+IN2=$(snmpget -v 1 -c $COMUNITY $IP IF-MIB::ifInOctets.$MIB | awk '{print $4}')
+
 
 # Diminuindo
 OUTM=$(($OUT2-$OUT1))
@@ -17,4 +22,5 @@ INM=$(($IN2-$IN1))
 TOTALO=$((($OUTM * 8)/$INTERVAL))
 TOTALI=$((($INM * 8)/$INTERVAL))
 
-echo $TOTALI $TOTALO >> logs.txt
+# Dumpe para algum arquivo !
+echo $TOTALI $TOTALO 
